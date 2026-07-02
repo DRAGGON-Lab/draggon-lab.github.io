46c88bfd3ca992bc687d9ed52ce76143238da45c
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2026-07-02 11:04:29 -0600

    Add site configuration

---
+++ src/data/siteConfig.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  labName: 'DRAGGON Lab',
+  acronym: 'Developing, Researching, and Architecting Genetic and Genomic Networks',
+  canonicalUrl: 'https://draggonlab.org',
+  affiliation: 'University of Bristol - School of Engineering Mathematics and Technology',
+  contactEmail: 'contact@draggonlab.org',
+  githubOrg: 'https://github.com/DRAGGON-Lab',
+  websiteSource: 'https://github.com/DRAGGON-Lab/draggon-lab.github.io',
+  googleScholar: 'https://scholar.google.com/citations?user=m3sQS9cAAAAJ&hl=en',
+  linkedin: 'https://www.linkedin.com/'
+} as const;
